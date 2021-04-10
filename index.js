502b1c665ce01d389c6d5c04a4b32bab0b112744
bclarke329 <bmendoza329@hotmail.com>
2021-04-10 13:33:12 -0500

    commit tree 3dfb19d35fd68a16303ced34f4d98d01104f2ccc

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+const template = require("babel-template")
+
 // Your code here
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(x => x * -1)
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map(x => x)
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(x => x * 2)
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(x => x ** 2)
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) {
+    let total = startingPoint
+    for (let i = 0; i< sourceArray.length; i++) {
+        total = total + sourceArray[i]
+    }
+    return total 
+}
+
+function reduceToAllTrue(sourceArray) {
+    for(let i = 0; i < sourceArray.length; i++) {
+        if (!sourceArray[i])
+        return false
+    }
+    return true
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for(let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i])
+        return true
+    }
+    return false
+}
